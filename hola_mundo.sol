60640e86235b4c3f7ba24122ed1c83c180a76ab5
Elizabeth5030 <elizabethscondori2@gmail.com>
2024-11-03 17:57:21 -0400

    primer contrato

---
+++ hola_mundo.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+//0xf5209EdbBAd6973960EA8308d819F82828DCd795
+
+pragma solidity 0.8.24; // en esta parte defino la version del compilador
+
+// a partir de aca coloco la logica de mi contrato
+contract hola_mundo {
+
+    string public saludo = "Hola desde Educateth 2.0"; // declarando el contenido de mi variable saludo
+
+}
